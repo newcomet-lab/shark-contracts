5f016b10a3a3127ca962ca5bce5ed3fb118a3cc1
newcomet-lab <newcometcto729@gmail.com>
2022-04-13 11:14:09 +0900

    added scripts

---
+++ scripts/8-deploy-wsSHRK.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const sShrkERC20 = require('../abi/StakedShark.json');
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log(`Deploying contracts with the account: ${deployer.address}`);
+
+    const balance = await deployer.getBalance();
+    console.log(`Account balance: ${balance.toString()}`);
+
+    const sSHRK = new ethers.Contract(sShrkERC20.address, sShrkERC20.abi, deployer);
+    console.log(`sSHRK address: ${sSHRK.address}`);
+
+    const contractFactory = await ethers.getContractFactory('wsSHRK');
+    const contract = await contractFactory.deploy(sSHRK.address);
+    console.log(`Contract address: ${contract.address}`);
+
+    const data = {
+        address: contract.address,
+        abi: JSON.parse(contract.interface.format('json'))
+    };
+    fs.writeFileSync('abi/wsSHRK.json', JSON.stringify(data));
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((e) => {
+        console.log(e);
+        process.exit(1);
+    });
